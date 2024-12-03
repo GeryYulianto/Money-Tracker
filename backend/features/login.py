efbc3dc8e56e203489fe843a8d56a796bcced021
Gery <fsygeryy@gmail.com>
2024-12-03 13:21:03 +0700

    add setup

---
+++ backend/features/login.py
@@ -0,0 +1,12 @@
+from flask_app import FlaskApp
+from flask import *
+
+class loginFeatures(FlaskApp):
+    def login(self):
+        if request.method == 'POST':
+            pass
+        else:
+            pass
+
+    def add_ednpoint_login(self):
+        self.add_endpoint('/login', 'login', self.login, ['GET', 'POST'])
