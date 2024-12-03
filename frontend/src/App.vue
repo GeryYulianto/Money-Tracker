4a6c018a469b1daef18e480e97368c3ba7c360b0
Gery <fsygeryy@gmail.com>
2024-12-03 13:34:36 +0700

    vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import login from './components/login/login.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <login msg="LOGIN" />
 </template>
 
 <style scoped>
