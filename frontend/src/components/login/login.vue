4c6d7244895cb0f1eecb7d884fcb5ad58557613c
JoFebriann <jofebrian240@gmail.com>
2024-12-03 14:11:05 +0700

    bootstrap

--- frontend/src/components/login/login.vue
+++ frontend/src/components/login/login.vue
@@ -1,44 +1,41 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
+// defineProps({
+//   msg: {
+//     type: String,
+//     required: true,
+//   },
+// })
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+    <h1 class="text-success text-center"> Login </h1>
+    
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+
+                        <form id="registrationForm" action="">
+                            <div class="form-group">
+                                <label for="email"> Email </label>
+                                <input type="email" class="form-control" id="email" placeholder="Email" required />
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password"> Password </label>
+                                <input type="password" class="form-control" id="password" placeholder="Password" required />
+                            </div>
+
+                            <br>
+                            <button class="btn btn-primary"> Login </button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
