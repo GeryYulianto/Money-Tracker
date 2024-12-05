fa409ca7cf2a2d870363f9e1d401f10edac09ef0
Gery <fsygeryy@gmail.com>
2024-12-05 17:33:51 +0700

    add category & transaction + args

--- backend/features/login.py
+++ backend/features/login.py
@@ -1,16 +1,24 @@
 from flask_app import FlaskApp
 from flask import *
-from flask_session import Session
+from database.database import query_db
 
 class LoginFeatures(FlaskApp):
     def __init__(self, app):
         super().__init__(app)
     def login(self):
         if request.method == 'POST':
-            request.form.get('username')
-            
-        else:
-            pass
+            data = request.json
+            username = data.get('username')
+            password = data.get('password')
+            user = query_db('''
+                SELECT * FROM user
+                WHERE username = ? AND password = ?
+            ''', (username, password))
+            if user:
+                session['username'] = username
+                return jsonify('Success'), 200
+            else:
+                return jsonify('Wrong'), 404
 
     def add_endpoint_login(self):
         self.add_endpoint('/login', 'login', self.login, ['GET', 'POST'])
--- backend/features/workspace.py
+++ backend/features/workspace.py
@@ -6,15 +6,67 @@ from flask_session import Session
 class WorkSpaceFeatures(FlaskApp):
     def __init__(self, app):
         super().__init__(app)
-    def get_transactions(self):
-        transactions = query_db('''
-            SELECT * FROM transactions
-            WHERE username = ?
-        ''', ('admin',))
-
-        transactions_list = [dict(row) for row in transactions]
-
-        return jsonify(transactions_list)
     
+    def transactions(self):
+        if request.method == 'GET':
+            start_date = request.args.get('start_date')
+            end_date = request.args.get('end_date')
+            query = '''
+                SELECT * FROM transactions
+                WHERE username = ?
+            '''
+            params = [session.get('username')] if session.get('username') else ['admin']
+
+            if start_date:
+                query += ' AND date >= ?'
+                params.append(start_date)
+            
+            if end_date:
+                query += ' AND date <= ?'
+                params.append(end_date)
+
+            transactions = query_db(query, params)
+            print(query, params)
+
+            transactions_list = [dict(row) for row in transactions]
+
+            return jsonify(transactions_list)
+        elif request.method == 'POST':
+            pass
+    
+    def category(self):
+        utilities = request.args.get('utilities')
+        education = request.args.get('education')
+        entertainment = request.args.get('entertainment')
+        food = request.args.get('food')
+        health = request.args.get('health')
+
+        query = '''
+            SELECT * FROM category
+            '''
+
+        params = []
+        if utilities:
+            params.append(1)
+        if education:
+            params.append(2)
+        if entertainment:
+            params.append(3)
+        if food:
+            params.append(4)
+        if health:
+            params.append(5)
+
+        if params:
+            placeholders = ','.join(['?'] * len(params))
+            query += f' WHERE category_id IN ({placeholders})'
+
+        categories = query_db(query, params)
+        category_list = [dict(row) for row in categories]
+
+        return jsonify(category_list)
+        
+
     def add_workspace_endpoints(self):
-        self.add_endpoint('/transactions', 'transactions', self.get_transactions, ['GET'])
+        self.add_endpoint('/transactions', 'transactions', self.transactions, ['GET', 'POST'])
+        self.add_endpoint('/category', 'category', self.category, ['GET'])
